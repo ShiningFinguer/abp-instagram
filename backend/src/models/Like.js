29440d2845b4ba499e02bf1e55ea73a7447174e8
Clase Qihao <a220602ql@correu.escoladeltreball.org>
2025-10-23 18:45:15 +0200

    Crud Post V1

---
+++ backend/src/models/Like.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const LikeSchema = new mongoose.Schema({
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model('Like', LikeSchema);
