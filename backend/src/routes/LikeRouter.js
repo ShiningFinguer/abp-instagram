0bb0932559b6b06065048f3bae33de2be06699d9
Luis Sebastian Oliver Zambrano <a220195lo@correu.escoladeltreball.org>
2025-11-10 19:43:21 +0000

    LikeRouter corregido para que la API funcione

--- backend/src/routes/LikeRouter.js
+++ backend/src/routes/LikeRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { countLikes, toggleLike } from "../controllers/LikeController";
+import { countLikes, toggleLike } from "../controllers/LikeController.js";
 
 export const LikeRouter = express.Router();
 
