4c8353bcb4e24cbd2ad255b52eae07d031dce344
Rhian Estante <a220064re@correu.escoladeltreball.org>
2025-11-11 19:26:23 +0100

    add own profile add add

--- frontend/app/src/pages/Profile/ProfileHeaderOwn.jsx
+++ frontend/app/src/pages/Profile/ProfileHeaderOwn.jsx
@@ -57,44 +57,24 @@ const ProfileHeader = ({}) => {
   }
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '1rem',
-        padding: '2rem',
-      }}
-    >
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem", padding: "2rem" }}>
       {/*Profile Picture */}
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <img
-          src={userProfile?.profilePicURL || 'https://i.pravatar.cc/300'}
+          src={userProfile?.profilePic}
           alt="Profile"
-          style={{ width: '150px', height: '150px', borderRadius: '50%' }}
+          style={{ width: "150px", height: "150px", borderRadius: "50%" }}
         />
       </div>
 
       {/* Username and Follow */}
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '1rem',
-        }}
-      >
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
         <h2>{userProfile?.username}</h2>
-<<<<<<< HEAD
-        <button onClick={handleFollowUser}>
-          {isFollowing ? 'Unfollow' : 'Follow'}
-        </button>
-=======
         <HandleIsOwnProfile/>
->>>>>>> 6303fd8 (add own profile)
       </div>
 
       {/* Profile Info */}
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '2rem' }}>
+      <div style={{ display: "flex", justifyContent: "center", gap: "2rem" }}>
         <div>
           <strong>userProfile?.posts?.length</strong> Posts
         </div>
@@ -112,7 +92,7 @@ const ProfileHeader = ({}) => {
       {/* Bio */}
       <div style={{ textAlign: "center" }}>{userProfile?.bio}</div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileHeader
+export default ProfileHeader;
